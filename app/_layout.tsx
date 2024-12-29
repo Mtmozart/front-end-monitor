b573a50047021869ad69a1370e807429fdd71c8f
matheus mozart <mmsnborges@gmail.com>
2024-12-28 22:24:43 -0300

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,33 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: 'Início',
+            title: 'Bem vindo!',
+            drawerIcon: () => (
+              <Ionicons name="home-outline" size={18} color="#3A98FF" />
+            ),
+          }}
+        />
+
+        <Drawer.Screen
+          name="primeiro"
+          options={{
+            drawerLabel: 'Primeiro',
+            title: 'Primeiro componente!',
+            drawerIcon: () => (
+              <Ionicons name="heart-circle-outline" size={18} color="#3A98FF" />
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+}
---
+++ app/primeiro.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "@/constats/styles";
+
+export default function Primeiro() {
+  return (
+    <View style={styles.centralizado}>
+      <Text>primeiro</Text>
+    </View>
+  )
+}
